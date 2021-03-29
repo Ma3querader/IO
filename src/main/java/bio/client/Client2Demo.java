98664c28eb1526d0eede36ee8f345a7bfb22edfc
Ma3querader <593661332@qq.com>
2021-03-29 17:50:09 +0800

    bio多线程版

---
+++ src/main/java/bio/client/Client2Demo.java
@@ -0,0 +1,31 @@
+package bio.client;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * @Author: panyusheng
+ * @Date: 2021/3/29
+ * @Version 1.0
+ */
+public class Client2Demo {
+    public static void main(String[] args) throws IOException {
+        System.out.println("客户端启动...");
+        // 创建一个socket通信管道，请求与服务器8888端口连接
+        Socket socket = new Socket("127.0.0.1", 8888);
+        // 从Socket通信管道中得到一个字节输出流
+        OutputStream os = socket.getOutputStream();
+        // 把字节流改装成自己需要的流进行数据的发送
+        PrintStream ps = new PrintStream(os);
+        // 开始发送消息
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String msg = sc.nextLine();
+            ps.println(msg);
+            ps.flush();
+        }
+    }
+}
--- src/main/java/bio/server/ServerDemo.java
+++ src/main/java/bio/server/ServerDemo.java
@@ -13,18 +13,11 @@ public class ServerDemo {
     public static void main(String[] args) throws IOException {
         System.out.println("服务端启动....");
         ServerSocket serverSocket = new ServerSocket(8888);
-        // 在这里暂停等待接收客户端的连接,得到一个端到端的Socket管道
-        Socket socket = serverSocket.accept();
-        // 从Socket管道中得到一个字节输入流
-        InputStream is = socket.getInputStream();
-        // 把字节输入流包装成自己需要的流进行数据的读取
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
-        // 读取数据
-        String line;
-        while ((line = br.readLine()) != null) {
-            System.out.println("服务端收到："+line);
+        while (true) {
+            // 在这里暂停等待接收客户端的连接,得到一个端到端的Socket管道
+            Socket socket = serverSocket.accept();
+            new ServerReadThread(socket).start();
+            System.out.println(socket.getRemoteSocketAddress() + "上线了...");
         }
-
-
     }
 }
