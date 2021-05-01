d1e1caf418fddb7c8b04cc3e8b4ec7a289d5fc71
Ma3querader <593661332@qq.com>
2021-05-01 15:07:06 +0800

    protocol packet

---
+++ src/main/java/netty/test4/MyMessageDecoder.java
@@ -0,0 +1,27 @@
+package netty.test4;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ReplayingDecoder;
+import io.netty.util.CharsetUtil;
+import java.util.List;
+
+public class MyMessageDecoder extends ReplayingDecoder<Void> {
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        System.out.println(" 解码器 被调用");
+        //需要将得到二进制字节码-> MessageProtocol 数据包(对象)
+        int length = in.readInt();
+
+        byte[] content = new byte[length];
+        in.readBytes(content);
+
+        //封装成 MessageProtocol 对象，放入 out， 传递下一个handler业务处理
+        MessageProtocol messageProtocol = new MessageProtocol();
+        messageProtocol.setLen(length);
+        messageProtocol.setContent(content);
+
+        out.add(messageProtocol);
+
+    }
+}
---
+++ src/main/java/netty/test4/Server.java
@@ -0,0 +1,32 @@
+package netty.test4;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+
+/**
+ * @Author: panyusheng
+ * @Date: 2021/4/30
+ * @Version 1.0
+ */
+public class Server {
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new MyServerInitializer()); //自定义一个初始化类
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8888).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
